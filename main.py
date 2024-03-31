3c06897ba3b2c21d6bc6041c0083c14e39b92706
darsan012 <dsrsangautam@gmail.com>
2024-03-31 13:59:23 -0400

    entered the input to the search box

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+# for getting the keys
+from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome()
 
 #opens the chrome with the link entered
 driver.get("https://google.com")
+
+#selecting the input element of the google.com
+input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+input_element.send_keys("tech with tim" + Keys.ENTER) #entering the element to the input box
 
 # to hold the browser for 10 seconds
 time.sleep(10)
