23ce319ce34741fb22a1265cee6a6edc54a6d85f
darsan012 <dsrsangautam@gmail.com>
2024-03-31 14:23:48 -0400

    entered input and open link on the basis of the text

--- main.py
+++ main.py
@@ -2,20 +2,33 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 # for getting the keys
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 driver = webdriver.Chrome()
 
-#opens the chrome with the link entered
+# opens the chrome with the link entered
 driver.get("https://google.com")
 
-#selecting the input element of the google.com
+# Waiting for the response for 5 seconds
+WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "gLFyf"))
+)
+
+# selecting the input element of the Google.com
 input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
-input_element.send_keys("tech with tim" + Keys.ENTER) #entering the element to the input box
+input_element.clear()  # clearing the text field or input field
+input_element.send_keys("Darshan Gautam" + Keys.ENTER)  # entering the element to the input box
+
+# finding the element with the text
+WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.PARTIAL_LINK_TEXT, "Darshan Gautam - Advanced College of Engineering")))
+link = driver.find_element(By.PARTIAL_LINK_TEXT, "Darshan Gautam - Advanced College of Engineering")
+link.click()  # click on the link
 
 # to hold the browser for 10 seconds
 time.sleep(10)
 
-#closes the browser
+# closes the browser
 driver.quit()
 
